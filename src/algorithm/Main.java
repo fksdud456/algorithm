7de344b1cb5c2e43a6fa5c39f5650368a16dd0de
student <student@M14023>
2018-03-06 09:58:49 +0900

    add baekjoon template - Main class 2018/03/06

---
+++ src/algorithm/Main.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static int answer;
+
+	public static void main(String[] args) throws Exception {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int N = Integer.parseInt(br.readLine());
+
+		
+		System.out.println(answer);
+	}
+}
