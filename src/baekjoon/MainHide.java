52afd1f573364920870f84059e7866b41c9c131b
M140320\student <fksdud456@gmail.com>
2018-04-13 19:01:32 +0900

    Update baekjoon 1697. 숨바꼭질
    움직인 횟수를 저장하는 Queue를 없애고 visit에 저장하도록 수정
    180ms -> 120ms
    2018/04/13

--- src/baekjoon/MainHide.java
+++ src/baekjoon/MainHide.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class MainHide {
-	private static boolean visit[];
+	private static int visit[];
 	private static int answer;
 	private static int N, K;
 	private final static int MAXDIS = 100000;
@@ -19,48 +19,40 @@ public class MainHide {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 
-		visit = new boolean[MAXDIS + 1];
+		visit = new int[MAXDIS + 1];
 		answer = 0;
 
 		bfs(N);
-
 		System.out.println(answer);
 	}
 
+	//qCnt 
 	public static void bfs(int cur) {
 		Queue<Integer> q = new LinkedList<>();
-		Queue<Integer> qCnt = new LinkedList<>();
-
 		q.add(cur);
-		qCnt.add(0);
-
 		int tmp, tmpCnt;
 		while (!q.isEmpty()) {
 			tmp = q.remove();
-			tmpCnt = qCnt.remove();
-
-			System.out.println(tmp + ", " + tmpCnt);
-
-			visit[tmp] = true;
+			tmpCnt = visit[tmp];
 
 			if (tmp == K) {
 				answer = tmpCnt;
 				return;
 			}
 
-			if (tmp + 1 >= 0 && tmp + 1 <= MAXDIS && visit[tmp + 1] == false) {
+			if (tmp + 1 >= 0 && tmp + 1 <= MAXDIS && visit[tmp + 1] == 0) {
 				q.add(tmp + 1);
-				qCnt.add(tmpCnt + 1);
+				visit[tmp + 1] = (tmpCnt + 1);
 			}
 
-			if (tmp - 1 >= 0 && tmp - 1 <= MAXDIS && visit[tmp - 1] == false) {
+			if (tmp - 1 >= 0 && tmp - 1 <= MAXDIS && visit[tmp - 1] == 0) {
 				q.add(tmp - 1);
-				qCnt.add(tmpCnt + 1);
+				visit[tmp - 1] = (tmpCnt + 1);
 			}
 
-			if (tmp * 2 >= 0 && tmp * 2 <= MAXDIS && visit[tmp * 2] == false) {
+			if (tmp * 2 >= 0 && tmp * 2 <= MAXDIS && visit[tmp * 2] == 0) {
 				q.add(tmp * 2);
-				qCnt.add(tmpCnt + 1);
+				visit[tmp * 2] = (tmpCnt + 1);
 			}
 		}
 	}
