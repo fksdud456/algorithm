8292410320be22f4d36ce1c772a64a7c1d053897
fksdud456 <36909784+fksdud456@users.noreply.github.com>
2018-04-15 01:17:43 +0900

    Update MainExit.java

--- src/baekjoon/MainExit.java
+++ src/baekjoon/MainExit.java
@@ -1,9 +1,34 @@
-private static void moveBlue(Ball tmp) {
-		int tx = tmp.Bx + (dx[tmp.dir]);
-		int ty = tmp.By + (dy[tmp.dir]);
-
-		
+	private static void moveRed(Ball tmp) {
+		int tx = tmp.Rx + (dx[tmp.dir]);
+		int ty = tmp.Ry + (dy[tmp.dir]);
 		while (tx >= 0 && tx < N && ty >= 0 && ty < M) {
+			if (map[tx][ty] == EMPTY) {
+				tmp.Rx = tx;
+				tmp.Ry = ty;
+
+				tx = tmp.Rx + (dx[tmp.dir]);
+				ty = tmp.Ry + (dy[tmp.dir]);
+			} else if (map[tx][ty] == HOLE) {
+				tmp.Rx = tx;
+				tmp.Ry = ty;
+				break;
+			} else {
+				break;
+			}
+		}
+
+		tx = tmp.Bx + (dx[tmp.dir]);
+		ty = tmp.By + (dy[tmp.dir]);
+
+		while (tx >= 0 && tx < N && ty >= 0 && ty < M) {
+			if (tx == tmp.Rx && ty == tmp.Ry) {
+				if (map[tx][ty] == HOLE) {
+					tmp.Bx = tx;
+					tmp.By = ty;
+				}
+				break;
+			}
+
 			if (map[tx][ty] == EMPTY) {
 				tmp.Bx = tx;
 				tmp.By = ty;
@@ -12,30 +37,6 @@ private static void moveBlue(Ball tmp) {
 			} else if (map[tx][ty] == HOLE) {
 				tmp.Bx = tx;
 				tmp.By = ty;
-				break;
-			} else {
-				break;
-			}
-		}
-
-		tx = tmp.Rx + (dx[tmp.dir]);
-		ty = tmp.Ry + (dy[tmp.dir]);
-		while (tx >= 0 && tx < N && ty >= 0 && ty < M) {
-			if (tx == tmp.Bx && ty == tmp.By) {
-				if (map[tx][ty] == HOLE) {
-					tmp.Rx = tx;
-					tmp.Ry = ty;
-				}
-				break;
-			}
-			if (map[tx][ty] == EMPTY) {
-				tmp.Rx = tx;
-				tmp.Ry = ty;
-				tx = tmp.Rx + (dx[tmp.dir]);
-				ty = tmp.Ry + (dy[tmp.dir]);
-			} else if (map[tx][ty] == HOLE) {
-				tmp.Rx = tx;
-				tmp.Ry = ty;
 				break;
 			} else {
 				break;
